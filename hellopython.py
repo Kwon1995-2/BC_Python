64e374b61d3b0f3a3b398671dba23cdab7771782
Kwon1995-2 <67689901+Kwon1995-2@users.noreply.github.com>
2020-09-26 14:34:23 +0900

    Add files via upload

---
+++ hellopython.py
@@ -0,0 +1,19 @@
+print("hello python")
+
+#python exercise
+# logic = 1 + 2 + 3
+# int_a = 4
+# print(logic)
+# print(int_a)
+# print(3/2)
+# print(3*2)
+# print(3//2)
+# print(3%2)
+# print(3**2)
+#print(max(1,2, 3, 4, 5))
+#print(divmod(10,7))
+
+# from math import *
+# print(floor(5.678))
+# print(ceil(5.678))
+# print(sqrt(26))
