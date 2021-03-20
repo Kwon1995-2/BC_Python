8559be70de6e10f96f8114e22270506c3ba4e1fb
Kwon1995-2 <67689901+Kwon1995-2@users.noreply.github.com>
2021-03-20 17:39:49 +0900

    Add files via upload

---
+++ chapter8/exercise1.py
@@ -0,0 +1,11 @@
+#print(5/0) #zero division error
+# a = [0,1,2]
+# print(a[3]) #index error
+# fp = open("Nofile.txt", "r") #FileNotFounError
+# print(fp)
+# b = int(input("Type float number or stirng : "))#Value error
+# print(b)
+# print(1+var*2) #name error
+# print("str"+1) #Type Error
+#################################################################
+
