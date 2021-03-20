2dbf6675f502fc341e01879309f3975b4c7da83f
Kwon1995-2 <67689901+Kwon1995-2@users.noreply.github.com>
2021-03-20 17:37:16 +0900

    Add files via upload

---
+++ chapter1/ch1.py
@@ -0,0 +1,28 @@
+#print("hello python")
+#print("안녕 python")
+
+# print(2+2)
+# print(50-5*6)
+# print((50-5*6)/4)
+# print(8/5)
+# print(17//3)
+# print(17%3)
+# print(2**7)
+
+# x =2;
+# y=x*2+3
+# print(y)
+# print(Y)
+
+#논리적 명령행, 물리적 명령행
+# i = 10
+# print(i)
+# i=10;print(i);
+
+# i = 5
+#   j = 5
+
+#1
+print(3*2*1/2)
+#2
+print((5+3)*2*1/2);
