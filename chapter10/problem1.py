86d84819db2825721386c904ba4dea277e7dedcf
Kwon1995-2 <67689901+Kwon1995-2@users.noreply.github.com>
2021-03-20 22:50:30 +0900

    Delete chapter10 directory

--- chapter10/problem1.py
+++
@@ -1,4 +0,0 @@
-import socket
-
-print(socket.gethostbyname_ex('www.busan.ac.kr'))
-print(socket.gethostbyaddr('147.46.10.58'))
--- chapter10/problem2.py
+++
@@ -1,5 +0,0 @@
-import socket
-
-addrinfo = socket.getaddrinfo('www.naver.com', 80)
-#addrinfo = socket.getaddrinfo('www.dongyang.ac.kr', 80)
-print(addrinfo)
