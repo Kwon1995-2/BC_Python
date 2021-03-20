4cbb7d15f55cd757bae9c0d0ee104e6ace7567a5
Kwon1995-2 <67689901+Kwon1995-2@users.noreply.github.com>
2021-03-20 17:39:27 +0900

    Add files via upload

---
+++ chapter7/myModule.py
@@ -0,0 +1,17 @@
+# def sum(n):
+#     sum = 0
+#     for i in range(1, n + 1):
+#         sum += i
+#     return sum
+
+
+# def power(x, n):
+#     prod = 1
+#     for i in range(1, n + 1):
+#         prod = prod * x
+#     return prod
+
+# if __name__ == '__main__':
+#   print('{0}'.format(__name__))
+#   print(sum(5))
+#   print(power(5,2))
